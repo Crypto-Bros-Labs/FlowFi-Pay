ef819a5634ca047f04421037ae202c6ff127bcdc
Mane <angelillo3581@gmail.com>
2026-01-23 13:44:45 -0600

    Fix CLABE

--- src/shared/api/axiosService.ts
+++ src/shared/api/axiosService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_DEV_URL;
+const baseURL = import.meta.env.VITE_API_URL;
 
 // Instancia con autenticación
 const axiosInstance = axios.create({
